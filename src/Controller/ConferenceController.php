8caee39efabe224c4f593daffdd90351484d6976
Roman Kondrashov <nr.kondrashov@gmail.com>
2026-02-15 13:55:11 +0300

    Добавил страницы с отображением конференций и комментариев.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,26 +13,45 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ConferenceController extends AbstractController
 {
     // #[Route('/hello/{name}', name: 'homepage')]
-    #[Route('/hello', name: 'homepage')]
-    // public function index(string $name = ''): Response
-    public function index(Request $request): Response
+    // #[Route('/hello', name: 'homepage')]
+    // public function +index(string $name = ''): Response
+    // public function index(Request $request): Response
+    #[Route('/', name: 'homepage')]
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
+        // $greet = '';
 
-        if ($request->query->has('name')) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($request->query->get('name')));
-        }
+        // if ($request->query->has('name')) {
+        //     $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($request->query->get('name')));
+        // }
 
-        dump($request);
+        // dump($request);
 
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    $greet
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
-        );
+        // return new Response(<<<EOF
+        //     <html>
+        //         <body>
+        //             $greet
+        //             <img src="/images/under-construction.gif" />
+        //         </body>
+        //     </html>
+        //     EOF
+        // );
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+        ]);
     }
 }
